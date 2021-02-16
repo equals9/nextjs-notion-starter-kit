9502fe91b16ad400d05d13fa1bdadda3e78f9adb
equals9 <71198073+equals9@users.noreply.github.com>
2021-02-15 22:06:06 -0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,20 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '8c9c602bc12b4194bb7bd36fb3b5c5f3',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'sourcing',
+  domain: 'sourcd.xyz',
+  author: 'Chris Toler',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
-
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //description: 'Example site description',
+  //socialImageTitle: 'Transitive Bullshit',
+  //socialImageSubtitle: 'Hello World! 👋',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
